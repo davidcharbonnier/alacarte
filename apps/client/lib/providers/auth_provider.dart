e28ea48fc362f08a1f9f886b43081e0ba2d30f83
David Charbonnier <david.charbonnier@cloudimperiumgames.com>
2025-10-23 23:09:56 -0400

    feat(client): Upgrade riverpod dependency

--- apps/client/lib/providers/auth_provider.dart
+++ apps/client/lib/providers/auth_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 import 'package:flutter/foundation.dart';
 import '../models/user.dart';
 import '../services/api_service.dart';
